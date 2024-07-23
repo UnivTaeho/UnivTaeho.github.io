24db0d26314fab0fef32bf797887ac35201306a0
UnivTaeho <xogh080907@gmail.com>
2024-07-23 14:17:20 +0900

    asdf

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "a.h"
+#include "b.h"
+
+int main(void) {
+    hello();
+    puts("\n");
+    foo();
+
+    hello();
+    hello();
+    hello();
+    hello();
+    hello();
+    hello();
+    hello();
+    
+    return 0;
+}
